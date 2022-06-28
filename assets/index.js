2705edb9576147d59a44012a5e8c7998301e7fb9
theeduke <oscarian528@gmail.com>
2022-06-28 13:07:00 +0300

    a win

--- assets/index.js
+++ assets/index.js
@@ -8,23 +8,37 @@ var statusEl = document.querySelector(".status");
 var colors = [];
 createBlocks(n);
 resetGame();
+var correct = document.querySelector(".right");
+var pickedColor;
 
 function checkColors(e) {
   // your code here
+  if (colors[pickedColor] === e.target.style.backgroundColor) {
+    correct.innerHTML = "Yipee! You got it correct";
+    document.body.style.color = colors[pickedColor];
+    for (var i=0; i<colorsBlocks.length; i++) {
+      colorsBlocks[i].style.backgroundColor = colors[pickedColor];
+    }
+    resetGame();
+  }
+  else {
+    e.target.style.backgroundColor = "transparent";
+    correct.innerHTML = "Please try again!";
+  }
 }
 
 function resetGame() {
-  createBlocks(n);
-  document.body.style.color = "black";
-  colors = [];
-  pickColors();
-  pickedColor = random(n);
-  rgbEl.innerHTML = colors[pickedColor];
-  setColors();
-  statusEl.innerHTML =
-    "Try to guess the right color based on the RGB value by clicking on the blocks.";
+  setTimeout(function(){createBlocks(n);
+    document.body.style.color = "black";
+    colors = [];
+    pickColors();
+    pickedColor = random(n);
+    rgbEl.innerHTML = colors[pickedColor];
+    setColors();
+    statusEl.innerHTML =
+      "Try to guess the right color based on the RGB value by clicking on the blocks.";
+    },800);
 }
-
 function setColors() {
   for (var i = 0; i < colorsBlocks.length; i++) {
     colorsBlocks[i].style.backgroundColor = colors[i];
@@ -44,9 +58,18 @@ function randomColor() {
 function random(r) {
   return Math.floor(Math.random() * r);
 }
-
+for (var i=0;i< diffEls.length;i++){
+  diffEls[i].addEventListener('click',setNumberOfTiles);
+}
 function setNumberOfTiles(e) {
   // your code here
+  for (var i=0; i<diffEls.length; i++) {
+    diffEls[i].classList.remove("active");
+  }
+  e.target.classList.add("active");
+  diffEl = document.querySelector(".diff__btn.active").innerHTML;
+  n = diffEl;
+  resetGame();
 }
 
 function createBlocks(num) {
